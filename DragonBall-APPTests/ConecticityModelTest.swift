bfe9e4c420c7d93edf9030591b1d8465350d439d
Manuel Cazalla Colmenero <manuelcazallacolmenero@MacBook-Air-de-Manuel.local>
2024-07-04 18:40:00 +0200

    SwiftLine

--- DragonBall-APPTests/ConecticityModelTest.swift
+++ DragonBall-APPTests/ConecticityModelTest.swift
@@ -71,14 +71,14 @@ final class ConecivityModelTest: XCTestCase {
             id: "1",
             name: "Goku",
             description: "El Prota",
-            favorite: false, 
-            photo: "goku.jpg"),
-            Heroe(
-            id: "2",
-            name: "krilin",
-            description: "Mejor amigo de Son Goku",
             favorite: false,
-            photo: "Krilin.jpg")]
+            photo: "goku.jpg"),
+                                    Heroe(
+                                        id: "2",
+                                        name: "krilin",
+                                        description: "Mejor amigo de Son Goku",
+                                        favorite: false,
+                                        photo: "Krilin.jpg")]
         
         MockURLProtocol.requestHandler = { request in
             let data = try? JSONEncoder().encode(someHeroes )
@@ -106,38 +106,38 @@ final class ConecivityModelTest: XCTestCase {
         wait(for: [expectation])
         
     }
-   
-   }
     
-    final class MockURLProtocol: URLProtocol {
-        static var error: ConnectivityModel.ConectivityError?
-        static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse,Data))?
-        
-        override class func canInit(with request: URLRequest) -> Bool {
-            return true
-        }
-        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            return request
-        }
-        override func startLoading() {
-            if let error = MockURLProtocol.error {
-                client?.urlProtocol(self, didFailWithError: error)
-                return
-            }
-            guard let handler = MockURLProtocol.requestHandler else {
-                assertionFailure("Received unexpected request with no handler")
-                return
-            }
-            do {
-                let (response, data) = try handler(request)
-                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-                client?.urlProtocol(self, didLoad: data)
-                client?.urlProtocolDidFinishLoading(self)
-            }
-            catch{
-                client?.urlProtocol(self, didFailWithError: error)
-            }
-        }
-        override func stopLoading() {}
+}
+
+final class MockURLProtocol: URLProtocol {
+    static var error: ConnectivityModel.ConectivityError?
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse,Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
     }
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    override func startLoading() {
+        if let error = MockURLProtocol.error {
+            client?.urlProtocol(self, didFailWithError: error)
+            return
+        }
+        guard let handler = MockURLProtocol.requestHandler else {
+            assertionFailure("Received unexpected request with no handler")
+            return
+        }
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        catch{
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    override func stopLoading() {}
+}
 
